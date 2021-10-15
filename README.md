83bf5d0f0ad1ca87c2bcb5e921fd4b4dce79b75b
Spencer Blanden <89828067+spencerblanden@users.noreply.github.com>
2021-10-14 21:19:47 -0500

    Add Style and fix functionality, ready for deploy, add README

---
+++ README.md
@@ -0,0 +1,24 @@
+# BeThere
+
+Have you ever wanted to go a local spot and see some familiar faces but when you srrive no one is there? With BeThere you can plan out times to meet and connect with friends at your favorite spots. This app utilizes node.js and express to create a functional platform for you to build connections. The style and interface are easy to use thanks to the CDN Bulma. 
+
+# Technologies Used
+- HTML
+- CSS
+- Node.js
+- Express
+- JavaScript
+- Heroku
+- Bulma
+- Bcrypt
+
+# Screenshots
+![screenshot1](/public/Screenshot1.png)
+![screenshot2](/public/Screenshot2.png)
+![screenshot3](/public/Screenshot3.png)
+
+# Getting Started
+[Click here](https://betherebesquare.herokuapp.com/) to see the deployed project. Sign-up for a username and get started.
+
+# Future Enhancements
+The user will be able to mark that they have are planning to go to the event or that they have already arrived.
--- models/seeds/placeSeed.js
+++ models/seeds/placeSeed.js
@@ -7,7 +7,7 @@ module.exports = [
         '6166cdc4c3b6b2a176b04425',
     description: 'A time for general assembly students to study',
     events: [{ 
-        name: 'Tuesday Debugging Session',
+        name: 'Debugging Session',
         day: 'Tuesday',
         time: '5',
         creator: '6166cdc4c3b6b2a176b04425',
@@ -15,24 +15,24 @@ module.exports = [
         there: ["What", "am", "I", "Doing"],  
     },
     {
-        name: 'Wednesday Debugging Session',
-        day: 'Tuesday',
-        time: '5',
+        name: 'Happy Hour',
+        day: 'Friday',
+        time: 'After Presentations',
         creator: '6166cdc4c3b6b2a176b04425',
         attendees: ["What", "am", "I", "Doing"], 
         there: ["What", "am", "I", "Doing"],
     }],
-    type: 'Study Group',
+    type: 'Discord Class Channel',
     tags: ['learning', 'studying', 'spaghetti breakfast']
 },
    {
-    name: 'Pick-Up Basketball Court(Liberty Hill)', 
+    name: 'Pick-Up Basketball Court', 
     img: 'https://i.pinimg.com/564x/de/55/9d/de559d4ec43492c8e2a4a5c819cf8e1e.jpg',
     address: 'Co Rd 200, Liberty Hill, TX 78642',
     creator: '6166cdc4c3b6b2a176b04425',
     description: 'Outdoor basketball court.',
     events: [{
-        name: 'wednesday pick up',
+        name: 'Pick-Up (All-Skill',
         day: "Wednesday",
         time: '5',
         creator: '6166cdc4c3b6b2a176b04425',
@@ -40,14 +40,39 @@ module.exports = [
         there: ["What", "am", "I", "Doing"]
     },
     { 
-        name: 'Tuesday Debugging Session',
+        name: 'Pick-Up (Semi-Pro',
         day: 'Tuesday',
-        time: '5',
+        time: '7',
         creator: '6166cdc4c3b6b2a176b04425',
         attendees: ["What", "am", "I", "Doing"], 
         there: ["What", "am", "I", "Doing"],  
     }],
-    type: 'city park',
+    type: 'City Park Court',
     tags: ['basketball', 'park', 'pickup']
-   } 
+   },
+   {
+    name: 'Spaghetti Breakfast Restaurant', 
+    img: 'https://www.greenvalleygrill.com/wp-content/uploads/2020/07/Courtyard.jpg',
+    address: '1501 Broadway ave, Maryville, TN',
+    creator: '6166cdc4c3b6b2a176b04425',
+    description: 'Small Town Bistro that serves Spaghetti for breakfast',
+    events: [{
+        name: 'Dinner Party',
+        day: "Saturday",
+        time: '8',
+        creator: '6166cdc4c3b6b2a176b04425',
+        attendees: ["This", "is", "Ryan", "..."], 
+        there: ["What", "am", "I", "Doing"]
+    },
+    { 
+        name: 'Kahoot',
+        day: 'Tuesday',
+        time: '6',
+        creator: '6166cdc4c3b6b2a176b04425',
+        attendees: ["What", "am", "I", "Doing"], 
+        there: ["What", "am", "I", "Doing"],  
+    }],
+    type: 'Small Town Bistro',
+    tags: ['Spaghetti', 'breakfast', 'kahoot']
+   }  
 ]
